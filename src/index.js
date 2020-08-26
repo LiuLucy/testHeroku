d77876fdf1e4cad2fb84f33e1a2586b3935e97d0
LucienLiou <lucy933626@gmail.com>
2020-08-26 10:16:41 +0800

    restart

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <>
+        <h1>Hello Heroku</h1>
+    </>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
